03a5954e89b22230b768911907cc6bd35f429eb8
Atheeleon <poeticum.visum@gmail.com>
2019-12-09 00:33:13 -0300

    Update Setup Linux.sh

--- Setup Linux.sh
+++ Setup Linux.sh
@@ -1,22 +1,17 @@
 #!/bin/sh
 
 echo "##Iniciando o script...##"
-sleep 2
+sleep 1
 sudo apt update
 
-sleep 2
 echo "##Instalando o Plank##"
-sleep 2
 sudo apt install plank
-sleep 1
 cp -v /usr/share/applications/plank.desktop /home/$USER/.config/autostart
 plank
 
 sudo apt update
 
-sleep 2
 echo "##Instalando o Wine##"
-sleep 2
 sudo dpkg --add-architecture i386 
 wget -nc https://dl.winehq.org/wine-builds/winehq.key
 sudo apt-key add winehq.key
@@ -24,3 +19,14 @@ sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic ma
 sudo apt update
 sudo apt install --install-recommends winehq-stable
 
+sudo apt update
+
+echo "##Instalando o GitHub##"
+sudo apt install git
+sudo add-apt-repository ppa:git-core/ppa
+sudo apt update
+sudo apt upgrade
+git --version
+sleep 2
+
+echo "##Instalando o Visual Studio#
