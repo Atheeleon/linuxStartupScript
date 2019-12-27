7802afff3a67de93439057adc15bcaae2463eb2e
Atheeleon <poeticum.visum@gmail.com>
2019-12-27 17:25:49 +0000

    setupLinux.sh

--- setupLinux.sh
+++ setupLinux.sh
@@ -20,8 +20,8 @@ echo ""
 echo "##Instalando o Plank##"
 sleep 1
 sudo apt install plank
-cp -v /user/share/applications/plank.desktop /home/"$USER$"/.config/autostart
-plank
+cp -v /user/share/applications/plank.desktop /home/"$USER"/.config/autostart
+(plank &> /dev/null &)
 sleep 1
 echo "Plank instalado..."
 sleep 1
@@ -33,8 +33,10 @@ sleep 1
 echo ""
 echo "##Instaland o Wine##"
 sleep 1
-sudo snap install wine-platform-i386
-sudo snap install wine-platform-i386 --edge
+wget -nc https://dl.winehq.org/wine-builds/winehq.key
+sudo apt-key add winehq.key
+sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic mai
+sudo apt update && sudo apt install --install-recommends winehq-stable
 sudo apt update
 sleep 1
 echo "Snap instalado..."
@@ -52,14 +54,14 @@ sleep 1
 echo ""
 echo "##Tema mcOs e Icones"
 
-mkdir /home/"$USER$"/.themes
-mkdir /home/"$USER$"/.icons
+mkdir /home/"$USER"/.themes
+mkdir /home/"$USER"/.icons
 wget -c https://dllb2.pling.com/api/files/download/id/1570117779/s/3a997923ec870cc3ec74349757d862e02432c088bce9dfe019cc57b989d9ac852042e191915c349fefc0a19cc5fc8aced2cbf504cc00b6af98c5ae857903a59a/t/1577468957/c/3a997923ec870cc3ec74349757d862e02432c088bce9dfe019cc57b989d9ac852042e191915c349fefc0a19cc5fc8aced2cbf504cc00b6af98c5ae857903a59a/lt/download/McOS-CTLina-Mint-Dark.tar.xz
 tar -xf McOS-CTLina-Mint-Dark.tar.xz
-mv McOS-CTLina-Mint-Dark /home/"$USER$"/.themes/'mcOS Catalina Mint'
-wget -c https://github.com/keeferrourke/la-capitaine-icon-theme.git
+mv McOS-CTLina-Mint-Dark /home/"$USER"/.themes/'mcOS Catalina Mint'
+git clone https://github.com/keeferrourke/la-capitaine-icon-theme.git
 unzip la-capitaine-icon-theme-master.zip
-mv la-capitaine-icon-theme-master /home/"$USER$"/.icons/'La Capitaine'
+mv la-capitaine-icon-theme-master /home/"$USER"/.icons/'La Capitaine'
 sleep 1
 echo "Tema e Icones instalados..."
 sleep 1
