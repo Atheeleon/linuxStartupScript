5bbe051c5931a8a07dd8556f3608881a59859afb
Atheeleon <poeticum.visum@gmail.com>
2020-02-04 17:34:33 -0200

    Removed Wine

--- setupLinux.sh
+++ setupLinux.sh
@@ -32,22 +32,6 @@ sudo apt update
 sleep 1
 
 echo ""
-echo "##Instaland o Wine##"
-sleep 1
-sudo dpkg --add-architecture i386
-wget -nc https://dl.winehq.org/wine-builds/winehq.key
-sudo apt-key add winehq.key
-sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main'
-sudo apt update
-sudo apt install --install-recommends  winehq-stable
-(winecfg &> /dev/null &)
-sudo apt update
-sleep 1
-echo "Wine instalado..."
-sleep 1
-
-
-echo ""
 echo "##Instalando o Visual Code"
 sleep 1
 sudo snap install code --classic
